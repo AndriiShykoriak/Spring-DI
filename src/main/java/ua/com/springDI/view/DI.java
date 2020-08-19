097a9c15393f26b7b521bdde54b4bd76be0bbe03
AndriiShykoriak <andrii.shykoriak@gmail.com>
2020-08-19 20:54:06 +0300

    view printCar,printUser

---
+++ src/main/java/ua/com/springDI/view/DI.java
@@ -0,0 +1,19 @@
+package ua.com.springDI.view;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import ua.com.springDI.service.CarService;
+import ua.com.springDI.service.UserService;
+
+@Component
+public class DI {
+    @Autowired
+    CarService carService;
+    @Autowired
+    UserService userService;
+
+    public void printCarWithUser() {
+        carService.printCar();
+        userService.printUser();
+    }
+}
